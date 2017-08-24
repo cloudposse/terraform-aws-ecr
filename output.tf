6b0f9d53c1799cc9fb8e059cafcb55a36fd33512
Sergey Vasilyev <s2504s@gmail.com>
2017-08-24 21:24:32 +0300

    Fixed LICENSE

--- output.tf
+++ output.tf
@@ -2,11 +2,10 @@ output "arn" {
   value = "${aws_ecr_repository.default.arn}"
 }
 
-//
-//output "name" {
-//  value = "${aws_ecr_repository.default.name}"
-//}
-//
+output "name" {
+  value = "${aws_ecr_repository.default.name}"
+}
+
 output "registry_id" {
   value = "${aws_ecr_repository.default.registry_id}"
 }
@@ -16,9 +15,9 @@ output "repository_url" {
 }
 
 output "role_arn" {
-  value = "${aws_iam_role.role.arn}"
+  value = "${aws_iam_role.default.arn}"
 }
 
 output "role_name" {
-  value = "${aws_iam_role.role.name}"
+  value = "${aws_iam_role.default.name}"
 }

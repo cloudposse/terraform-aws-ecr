684af46581df89ad9e53d19080f932ebb1e23056
Igor Rodionov <goruha@gmail.com>
2018-06-29 14:42:05 +0600

    Address PR comments

--- output.tf
+++ output.tf
@@ -15,40 +15,40 @@ output "repository_name" {
 
 output "role_name" {
   value       = "${join("", aws_iam_role.default.*.name)}"
-  description = "Assume Role name to get access registry"
+  description = "Assume Role name to get registry access"
 }
 
 output "role_arn" {
   value       = "${join("", aws_iam_role.default.*.arn)}"
-  description = "Assume Role ARN to get access registry"
+  description = "Assume Role ARN to get registry access"
 }
 
 output "policy_login_name" {
   value       = "${aws_iam_policy.login.name}"
-  description = "IAM Policy name allow to login in ECR"
+  description = "The IAM Policy name to be given access to login in ECR"
 }
 
 output "policy_login_arn" {
   value       = "${aws_iam_policy.login.arn}"
-  description = "IAM Policy ARN allow to login in ECR"
+  description = "The IAM Policy ARN to be given access to login in ECR"
 }
 
 output "policy_read_name" {
   value       = "${aws_iam_policy.read.name}"
-  description = "IAM Policy name allow to pull images from ECR"
+  description = "The IAM Policy name to be given access to pull images from ECR"
 }
 
 output "policy_read_arn" {
   value       = "${aws_iam_policy.read.arn}"
-  description = "IAM Policy ARN allow to pull images from ECR"
+  description = "The IAM Policy ARN to be given access to pull images from ECR"
 }
 
 output "policy_write_name" {
   value       = "${aws_iam_policy.write.name}"
-  description = "IAM Policy name allow to push images to ECR"
+  description = "The IAM Policy name to be given access to push images to ECR"
 }
 
 output "policy_write_arn" {
   value       = "${aws_iam_policy.write.arn}"
-  description = "IAM Policy ARN allow to push images to ECR"
+  description = "The IAM Policy ARN to be given access to push images to ECR"
 }

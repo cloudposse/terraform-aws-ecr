89834333a5b16daac5cf07de33fef6a5e6fbfcbf
Igor Rodionov <goruha@gmail.com>
2019-02-05 23:34:18 +0300

    Fix output

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "registry_id" {
-  value       = "${aws_ecr_repository.default.registry_id}"
+  value       = "${join("", aws_ecr_repository.default.*.registry_id)}"
   description = "Registry ID"
 }
 
 output "registry_url" {
-  value       = "${aws_ecr_repository.default.repository_url}"
+  value       = "${join("", aws_ecr_repository.default.*.repository_url)}"
   description = "Registry URL"
 }
 
 output "repository_name" {
-  value       = "${aws_ecr_repository.default.name}"
+  value       = "${join("", aws_ecr_repository.default.*.name)}"
   description = "Registry name"
 }

b6bbd764f18e9c7a06f7bd54bc2932db5a60656b
Igor Rodionov <goruha@gmail.com>
2019-02-06 00:03:07 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ locals {
 
 module "label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
+  enabled    = "${var.enabled}"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"

ea613edf8c822b3d2fe0545d775f9fe2e6c4ed7a
Igor Rodionov <goruha@gmail.com>
2019-05-01 16:59:40 +0600

    Fix policy

--- variables.tf
+++ variables.tf
@@ -54,5 +54,5 @@ variable "tags" {
 variable "max_image_count" {
   type        = "string"
   description = "How many Docker Image versions AWS ECR will store"
-  default     = "1000"
+  default     = "999"
 }

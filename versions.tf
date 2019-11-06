c159b2b973f607c2bf3ff64984b60df334240cd5
Vadim Kadnikov <vadimka@scribd.com>
2019-11-06 19:07:50 +0100

    Set AWS provider minimum required version to v2.34.0

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 0.12.0"
 
   required_providers {
-    aws      = "~> 2.0"
+    aws      = "~> 2.34"
     template = "~> 2.0"
     local    = "~> 1.2"
     null     = "~> 2.0"

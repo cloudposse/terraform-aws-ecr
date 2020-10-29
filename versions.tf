37e51fed3c813dbcbc7ddcfac8c21f860938f000
PePe Amengual <jose.amengual@gmail.com>
2020-10-28 18:40:05 -0700

    Upgrading to AWS provider 3.x

--- versions.tf
+++ versions.tf
@@ -1,9 +1,17 @@
 terraform {
-  required_version = ">= 0.12.0"
-
+  required_version = ">= 0.13.3"
   required_providers {
-    aws      = ">= 2.34"
-    template = ">= 2.0"
-    local    = ">= 1.2"
+    aws = {
+      source = "hashicorp/aws"
+      version       = ">= 3.0, < 4.0"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = ">= 2.0"
+    }
+    local = {
+      source = "hashicorp/local"
+       version   = ">= 1.3"
+    }
   }
 }

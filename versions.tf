cbd635038f0a2e7dc224153d634305f43447adab
PePe Amengual <jose.amengual@gmail.com>
2020-10-29 20:38:28 -0300

    Update versions.tf
    Co-authored-by: Nuru <Nuru@users.noreply.github.com>

--- versions.tf
+++ versions.tf
@@ -1,17 +1,8 @@
 terraform {
   required_version = ">= 0.13.3"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.1"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.3"
-    }
+    aws      = ">= 3.1"
+    template = ">= 2.0"
+    local    = ">= 1.2"
   }
 }

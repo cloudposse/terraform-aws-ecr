a893ef8f4c8882b067b75d0ea97dede25ddc60cd
PePe Amengual <jose.amengual@gmail.com>
2020-10-28 22:45:05 -0300

    Update versions.tf
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version       = ">= 3.0, < 4.0"
+      version       = ">= 3.1"
     }
     template = {
       source = "hashicorp/template"

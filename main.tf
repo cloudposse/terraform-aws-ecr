fc6c25a859c0780f2c851244b326abd62838fad1
Kevin Pankonen <2014764+kpankonen@users.noreply.github.com>
2022-06-10 06:45:24 -0700

    Update main.tf
    Co-authored-by: nitrocode <7775707+nitrocode@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
-  principals_readonly_access_non_empty = length(var.principals_readonly_access) > 0 ? true : false
-  principals_push_access_non_empty     = length(var.principals_push_access) > 0 ? true : false
-  principals_full_access_non_empty     = length(var.principals_full_access) > 0 ? true : false
+  principals_readonly_access_non_empty = length(var.principals_readonly_access) > 0
+  principals_push_access_non_empty     = length(var.principals_push_access) > 0
+  principals_full_access_non_empty     = length(var.principals_full_access) > 0
   ecr_need_policy                      = length(var.principals_full_access) + length(var.principals_readonly_access) + length(var.principals_push_access) > 0 ? true : false
 }
 

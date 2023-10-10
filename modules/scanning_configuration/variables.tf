1a3f35134d13cc3a6f5ce8485b20d9b09971a15f
Jeremy White <jeremy.white@cloudposse.com>
2023-10-10 10:05:50 -0400

    fix(scanning_configuration): adjust scan_config type

--- modules/scanning_configuration/variables.tf
+++ modules/scanning_configuration/variables.tf
@@ -1,7 +1,7 @@
 variable "scan_config" {
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecr_registry_scanning_configuration
   description = "Scan configuration for ECR"
-  type = map(object({
+  type = object({
     scan_type = string
     rules = list(object({
       scan_frequency = string
@@ -10,7 +10,7 @@ variable "scan_config" {
         filter_type = optional(string, "WILDCARD")
       }))
     }))
-  }))
+  })
   validation {
     condition     = var.scan_config == null || contains(["ENHANCED", "BASIC"], var.scan_config.scan_type)
     error_message = "scan_type must be either ENHANCED or BASIC"
